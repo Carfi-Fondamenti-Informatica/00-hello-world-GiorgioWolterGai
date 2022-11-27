5223f3e28c5528fde3bda25afb76b90b0fc6ecc6
GiorgioWolterGai <118194394+GiorgioWolterGai@users.noreply.github.com>
2022-11-27 18:21:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 
 int main() {
-     int n=0 , a=1 , b=1 , c=0;
-    cin >> n;
-    cout << a << endl << b << endl;
-    for ( int i = 0; i<= n; i++){
-        c = a + b;
-        a = b;
-        b = c;
-        cout << c << endl;
-    }
+      int a=1, b=0, c=0, n=0; cin >> n;
+      if (n>0) {
+          cout << a << endl;
+          while (c<n) {
+             c=a+b;
+             b=a;
+             a=c;
+             if (c<=n) {
+                 cout << c << endl;
+             }
+          }
+      }
    return 0;
 }
